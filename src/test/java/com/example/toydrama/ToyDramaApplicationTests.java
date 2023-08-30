8ae1278f030da20142a64a9ff03d509a666ddde2
Soonrang <clearsbyul30@naver.com>
2023-08-31 01:17:14 +0900

    Initial commit

---
+++ src/test/java/com/example/toydrama/ToyDramaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.toydrama;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ToyDramaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
